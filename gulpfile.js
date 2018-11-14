cea7ca9733faeb7129b63e1bc74d75192ed5795c
Doanh Pham <doanhpham1995@gmail.com>
2018-11-13 20:22:01 -0500

    check

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,24 @@ const nodemon = require('gulp-nodemon');
 
 gulp.task('default', ['browser-sync'], function () {});
 
+gulp.task('nodemon', function (cb) {
+  let started = false;
+
+  return nodemon({
+    script: 'index.js'
+  }).on('start', function () {
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
+});
+
 gulp.task('browser-sync', ['nodemon'], function () {
   browserSync.init(null, {
     proxy: "http://localhost:5000",
     files: ["views/**/*.*"],
     browser: "google chrome",
     port: 7000,
-  });
-});
-gulp.task('nodemon', function (cb) {
-
-  let started = false;
-
-  return nodemon({
-    script: 'index.js'
-  }).on('start', function () {
-    // to avoid nodemon being started multiple times
-    // thanks @matthisk
-    if (!started) {
-      cb();
-      started = true;
-    }
   });
 });
